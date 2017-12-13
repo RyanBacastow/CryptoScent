2b9a669e42c986603be65523d8b5667bde0a830f
Swordyjohn <32469251+Swordyjohn@users.noreply.github.com>
2017-12-13 16:45:50 -0600

    Basic framework for reddit scraper, 2 of highest volume subreddits requests for top posts and hashed API data

--- RedditScrape.py
+++ RedditScrape.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
-params = {'t':'hour'}
-response = requests.get('http://www.reddit.com/r/CryptoCurrency/top/.json', params=params)
-read = response.json()
+##https://www.reddit.com/dev/api API endpoints and params
+params = {'t':'day'}
+response_crypto = requests.get('http://www.reddit.com/r/CryptoCurrency/top/.json',\
+                               params=params)
+response_trading = requests.get('http://www.reddit.com/r/CryptoCurrencyTrading/top/.json',\
+                               params=params)
+crypto_data = response_crypto.json()
 print(read)           
